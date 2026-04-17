29f98ab99d4258354eb11738a0bac4f5a30ff291
irfan vural <irfanvural.dev@gmail.com>
2026-04-17 12:56:09 +0300

    Initial Commit

---
+++ FlashAI/FlashAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashAIApp.swift
+//  FlashAI
+//
+//  Created by İrfan Vural on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
